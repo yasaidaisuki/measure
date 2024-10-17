a350f2be96af1fad9faf9a98145880abc5eb0e9d
yasaidaisuki <peng.dingkai@gmail.com>
2024-10-17 01:54:13 -0400

    added diffused graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import matplotlib.ticker as ticker
 
 fig, ax = plt.subplots()      
@@ -19,7 +20,19 @@ ax.yaxis.set_major_locator(ticker.MaxNLocator())
 plt.xscale('log')
 plt.xticks([0.1, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000], [0, 20, 50, 100, 200, 500, '1k', '2k', '5k', '10k', '20k'])
 plt.xlim(20,20000)
-df = pd.read_csv('./measurements/oratory1990/over-ear/Focal Elex.csv', usecols=header)  
+
+read = input("Display Diffused or Raw: ")
+
+if read == "Diffused": 
+    df = pd.read_csv('./targets/Diffuse field 5128.csv')
+    df2 = pd.read_csv('./measurements/oratory1990/over-ear/Focal Elex.csv')
+
+    # Ensure data is aligned by frequency before subtraction
+    df = pd.merge(df[['frequency', 'raw']], df2[['frequency', 'raw']], on='frequency', suffixes=('', '_compare'))
+    df['raw'] = df['raw'].sub(df['raw_compare'])
+  
+elif read == "Raw":
+    df = pd.read_csv('./measurements/oratory1990/over-ear/Focal Elex.csv', usecols=header)  
 
 plt.plot(df.frequency,df.raw)
 ax.grid(True)
